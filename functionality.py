abf4deb67ca26ac78f9c4d136e71c7abb092b6a3
Austin Daniell <adaniell@pm.me>
2020-09-19 20:39:38 -0400

    Fully functional

--- functionality.py
+++ functionality.py
@@ -2,15 +2,18 @@
 from twilio import twiml
 from twilio.rest import Client
 from datetime import datetime
+from curses.textpad import Textbox, rectangle
 import curses
+import time
 
 #Twilio info
 with open('/home/user/Projects/twilioAccountID.txt', 'r') as my_file:
         account_sid = my_file.read().rstrip()
 with open('/home/user/Projects/twilioAuth.txt', 'r') as my_file:
         auth_token = my_file.read().rstrip()
-toNumber = "+12487205123"
-fromNumber = "+12489189172"
+myNumber = "+12487205123"
+fromNumber = "+12487205123"
+toNumber = "+12489189172"
 phoneBook = {"+12489189172": "Sal", "+12487205123": "Austin"}
 recieved_from_numbers = []
 unique_from_numbers = []
@@ -23,6 +26,7 @@ convoHeight = 0
 messageWidth = 0
 messageHeight = 0
 current_contact_idx = 0
+messageString = ""
 
 client = Client(account_sid, auth_token)
 
@@ -36,18 +40,21 @@ def setFromNumber(num):
 def getFromNumber():
     return fromNumber
 
-def composeMessage():
-    messageBody=input("Message to send: ")
+def setToNumber(num):
+    global toNumber
+    toNumber = unique_from_numbers[num]
 
-def sendMessage():
+def newContactMessage
+
+def sendMessage(messageBody):
     message = client.messages.create(
-        to=toNumber, 
-        from_= fromNumber,
+        to= str(toNumber),
+        from_= str(myNumber),
         body=messageBody)
     print(message.sid)
 
 #from https://www.geeksforgeeks.org/python-get-unique-values-list/
-def unique(list1): 
+def unique(list1):
     # insert the list to the set 
     list_set = set(list1) 
     # convert the set to the list 
@@ -60,41 +67,54 @@ def unique(list1):
         #    unique_from_numbers.append(x)
         unique_from_numbers.append(x)
 
-
 def getConvoNumbers():
     global recieved_from_numbers
     for sms in client.messages.list():
         recieved_from_numbers.append(sms.from_)
         recieved_from_numbers.append(sms.to)
     unique(recieved_from_numbers)
-
-
+    unique_from_numbers.remove(myNumber)
 
 def getMessages():
     global contact_messages
+    contact_messages = []
     for sms in client.messages.list():
-        if (sms.to == fromNumber) | (sms.from_ == fromNumber):
+        if (sms.to == toNumber) | (sms.from_ == toNumber):
             sender = sms.from_
             target = sms.to
             message = sms.body
             timestamp = sms.date_sent
             contact_messages.append((timestamp.strftime("%m/%d/%Y, %H:%M:%S"), sender, target, message))
 
+def composeMessage(myscreen, selected_contact):
+    messageWindow = curses.newwin(messageHeight, messageWidth, convoHeight +1, contactWidth + 1)
+    messageWindow.addstr(0, 0, "Enter IM message: (hit Ctrl-G to send)")
+    rectangle(myscreen, messageHeight+1, messageWidth+ 1, convoHeight +1, contactWidth + 1)
+    #messageContent = "Send a message to " + str(selected_contact)
+    message_box = Textbox(messageWindow)
+    new_message = message_box.gather()
+
+    myscreen.getkey()
+    #messageWindow.addstr(1,1, messageContent)
+
 def convoWindow(myscreen, selected_contact):
     convoWindow = curses.newwin(convoHeight, convoWidth, 1, contactWidth + 1)
-    #convoWindow.clear()
+    convoWindow.clear()
     convoWindow.border(0)
     x = 0
     y = 0
     for idx, current_message in enumerate(reversed(contact_messages)):
-        x = 1
+        x = 2
         y = 1 + idx
         if y >= convoHeight:
             y = convoHeight - 1
             convoWindow.clear()
             convoWindow.refresh()
         timestamp = current_message[0]
-        sender = current_message[1]
+        if current_message[1] in phoneBook:
+            sender = phoneBook[current_message[1]]
+        else:
+            sender = current_message[1]
         target = current_message[2]
         message = current_message[3]
         formated_message = timestamp +" | "+sender+ ":" + message
@@ -103,8 +123,10 @@ def convoWindow(myscreen, selected_contact):
         except:
             convoWindow.clear()
             continue
-    #contactHeader = unique_from_numbers[selected_contact]
-    contactHeader = fromNumber
+    if toNumber in phoneBook:
+        contactHeader = phoneBook[toNumber]
+    else:
+        contactHeader = toNumber
     convoWindow.addstr(0,0,contactHeader)
     myscreen.refresh()
     convoWindow.refresh()
@@ -112,8 +134,8 @@ def convoWindow(myscreen, selected_contact):
 def messageWindow(myscreen, selected_contact):
     messageWindow = curses.newwin(messageHeight, messageWidth, convoHeight +1, contactWidth + 1)
     messageWindow.border(0)
-    messageContent = "Send a message to " + str(selected_contact)
-    messageWindow.addstr(1,1, messageContent)
+    messageContent = "Send a message to " + unique_from_numbers[selected_contact]
+    #messageWindow.addstr(1,1, messageContent)
     myscreen.refresh()
     messageWindow.refresh()
 
@@ -140,6 +162,8 @@ def contactsWindow(myscreen, selected_contact):
     contactsWindow.refresh()
 
 def mainDisplay(myscreen):
+    global fromNumber
+    global MessageWindow
     global recieved_from_numbers
     global messageWidth
     global messageHeight
@@ -148,11 +172,13 @@ def mainDisplay(myscreen):
     global contactHeight
     global contactWidth
     global current_contact_index
+    editWidth = curses.COLS - 5
+    editHeight = curses.LINES - 5
     contactHeight = curses.LINES - 2
     contactWidth = 15
     messageWidth = curses.COLS - contactWidth - 2 
     messageHeight = 5
-    convoHeight = curses.LINES - messageHeight - 2
+    convoHeight = curses.LINES - messageHeight - 3
     convoWidth = curses.COLS - contactWidth - 2
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
@@ -162,37 +188,62 @@ def mainDisplay(myscreen):
     contactsWindow(myscreen, current_contact_idx)
     messageWindow(myscreen, current_contact_idx)
     convoWindow(myscreen, current_contact_idx)
-    #Contacts Window
-    #contactsWindow.addstr(1,1,"Test")
-    #myscreen.refresh()
-    #contactsWindow.refresh()
-    #myscreen.attroff(curses.color_pair(1))
     curses.napms(2000)
     while 1:
         key = myscreen.getch()
         myscreen.clear()
-        if key == curses.KEY_UP and current_contact_idx > 0:
+        if key == curses.KEY_ENTER or key in [10,13]:
+            FromNumber=myNumber
+            setToNumber(current_contact_idx)
+            myscreen.addstr(0, 0, "Enter IM message: (hit Ctrl-G to send)")
+
+            editwin = curses.newwin(editHeight , editWidth, 2,1)
+            rectangle(myscreen, 1,0, 1+editHeight+1, 1+editWidth+1)
+            myscreen.refresh()
+
+            box = Textbox(editwin)
+
+            # Let the user edit until Ctrl-G is struck.
+            box.edit()
+
+            # Get resulting contents
+            message = box.gather()
+            sendMessage(message)
+
+            #myscreen.getkey()
+            myscreen.clear()
+            myscreen.refresh()
+        elif key == curses.KEY_UP and current_contact_idx > 0:
             current_contact_idx -=1
-            setFromNumber(current_contact_idx)
+            setToNumber(current_contact_idx)
+            getMessages()
+        elif key == ord('K') and current_contact_idx > 0:
+            current_contact_idx -=1
+            setToNumber(current_contact_idx)
+            getMessages()
+        elif key == ord('J') and current_contact_idx < len(unique_from_numbers)-1:
+            current_contact_idx +=1
+            setToNumber(current_contact_idx)
             getMessages()
         elif key == curses.KEY_DOWN and current_contact_idx < len(unique_from_numbers)-1:
             current_contact_idx +=1
-            setFromNumber(current_contact_idx)
+            setToNumber(current_contact_idx)
             getMessages()
+        elif key == ord('q'):
+            break
 
         myscreen.refresh()
         convoWindow(myscreen, current_contact_idx)
         contactsWindow(myscreen, current_contact_idx)
         messageWindow(myscreen, current_contact_idx)
 
-    
 def callDisplay():
     curses.wrapper(mainDisplay)
 
-
 def main():
-    getMessages()
     getConvoNumbers()
+    setFromNumber(current_contact_idx)
+    getMessages()
     callDisplay()
 
 main()
